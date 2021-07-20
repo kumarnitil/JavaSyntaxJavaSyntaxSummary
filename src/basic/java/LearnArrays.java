b48eabdbf5777e3a7138da40dbfbcd03bb3d6709
kumarnitil <38610712+kumarnitil@users.noreply.github.com>
2021-07-20 23:08:44 +0530

    Update LearnArrays.java

--- src/basic/java/LearnArrays.java
+++ src/basic/java/LearnArrays.java
@@ -1,9 +1,19 @@
-package basic.java;
+package basics.java;
 
 public class LearnArrays {
-
+    /**
+     * The limitations with Array is - 
+     * 1.> You can store variables of one data type.
+     * 2.> Once the size of the array has been initialized it cannot be changed.
+     * 3.> Index always starts with 0
+     * 4.> intArr.length will give the length of the Array
+     * @param args
+     */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//System.out.println(intArr);//This will be just print memory address
+		//System.out.println(intArr[4]);// This will print the element at location index 5
 
 	}
 
