9e6e4f2c2f387516ead8afbceff320486668eda6
kumarnitil <38610712+kumarnitil@users.noreply.github.com>
2021-08-07 08:02:32 +0530

    Update Calculator.java

--- src/demo/calcengine/Calculator.java
+++ src/demo/calcengine/Calculator.java
@@ -1,9 +1,33 @@
 package demo.calcengine;
 
-public class Calculator {
+public class calculator {
+	
+	/*
+	 * Creation of simple calculator which would do
+	 * +, -, /, %
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double value1 = 100.0d;
+        double value2 = 0.0d;
+        double result = 0.0d;
+        char opcode = 'f';
+        
+        if(opcode=='a')
+        	result = value1+value2;
+        else if(opcode=='s')
+        	result = value1-value2;
+        else if(opcode=='m')
+        	result = value1*value2;
+        else if(opcode=='d') {
+        	if(value2 != 0)
+        	result = value1/value2;
+        }
+        else {
+        	System.out.println("Invalid Opcode "+opcode);
+        	result = 0.0d;
+        }
+        
 
 	}
 
