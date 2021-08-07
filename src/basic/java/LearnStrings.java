17de95796ee431456b3ae71b5e0b508dc7f771da
kumarnitil <38610712+kumarnitil@users.noreply.github.com>
2021-08-07 08:01:16 +0530

    Update LearnStrings.java

--- src/basic/java/LearnStrings.java
+++ src/basic/java/LearnStrings.java
@@ -1,10 +1,50 @@
-package basic.java;
+package basics.java;
 
-public class LearnStrings {
+public class LearnMethods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * In Java Parameters are passed by value 
+		 * Parameters receives receives a copy of original value.
+		 * Thus the method changes to parameter values are within the method and not visible to outside of methods
+		 * There are three why a method would end
+		 * - there is no more code left to execute
+		 * - Using a return statement as simple as return; can also work
+		 * - Error occurred during execution
+		 * 
+		 * A method can return a single value
+		 * - it can be a primitive type
+		 * - it can be an array
+		 * 
+		 * Curly braces are 
+		 */
+		
+		justAnotherMethod();//function call without any parameters
+		findsum(2.0f,3.0f,2);//function call with three arguments
 	}
 
+	static void justAnotherMethod() {
+		System.out.println("Just a function call");
+	}
+	
+	static void findsum(float x, float y, int count) {
+		if(count == 0)
+			return;//execution of the return statement in between to stop function/method body execution
+		for(int i=1;i<=count;i++)
+		System.out.printf("%d.> Printing the sum of x + y = %f\n",i,x+y);
+		
+	}
+	
+	/*
+	 * In Java method parameter are always passed by value and the next swap function will depict that.
+	 */
+      
+	
+	static void beforeswap(int org1, int org2) {
+		
+	}
+	
+	static void swap(int swap1, int swap2) {
+		
+	}
 }
