b5a6a023f8330ff1a3a560d677a08ae1f26d3cae
kumarnitil <38610712+kumarnitil@users.noreply.github.com>
2021-08-07 07:58:06 +0530

    Update LearnBlockandSwitch.java

--- src/basic/java/LearnBlockandSwitch.java
+++ src/basic/java/LearnBlockandSwitch.java
@@ -1,9 +1,27 @@
-package basic.java;
+package basics.java;
 
 public class LearnBlockandSwitch {
+	/*
+	 * Block Statement
+	 * 1.> Block statement are defined using opening and closing braces- {}
+	 * 2.> Variable declared within the block statement
+	 *     - Scope limited to that block.
+	 *     - Variable is not visible outside the block.
+	 */
+	
+	/*
+	 * Switch Case Statement
+	 * The primitive data types supported by Switch case are limited are : - 
+	 * byte
+	 * short
+	 * int
+	 * long
+	 * char
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Refer to demo.calcengine.calculator.java to see the demo of block statement.
+		//Refer to demo.calcengine.CalculatorUsingSwitch.java to see the demo of switch statement
 
 	}
 
