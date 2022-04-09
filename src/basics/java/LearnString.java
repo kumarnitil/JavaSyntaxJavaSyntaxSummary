da4237a3e498e1f64d304eb94793487baf1b7a14
Kumar Nitil <kumarnitil@gmail.com>
2022-04-09 13:31:59 +0530

    String Conversion

--- src/basics/java/LearnString.java
+++ src/basics/java/LearnString.java
@@ -6,11 +6,38 @@ public class LearnString {
         String B = "Nitil";
         String C = "Kumar Nitil";
         String D = A+B;
+        String E = C.intern();//Will create the intern version of the string C
+        String F = C.intern();//Will point F to the same interned version which means same reference and content
         if(C == D){
-            System.out.print("True");//Will compare the reference where the strings are stored
+            System.out.println("True");//Will compare the reference where the strings are stored
         }
         if(C.equals(D)){
-            System.out.print(" Equals True");//Will compare the contents of the Strings
+            System.out.println(" Equals True");//Will compare the contents of the Strings
         }
+        if(!C.equals(D)){
+            System.out.println(" Equals True");//Will compare the not equal contents of the Strings
+        }
+        else{
+            System.out.println("Not !C is used to compare not equal of string contents");
+        }
+        if(E == F){
+            System.out.println("Pointing to same reference Both the cases True");//Will compare the reference where the strings are stored
+        }
+        if(E.equals(F)){
+            System.out.println("Same contents Both the cases True");//Will compare the reference where the strings are stored
+        }
+
+        /*
+        String Conversion
+        Any datatype can be converted into a string using ValueOf Method.
+        Also there is a implicit conversion of string which a compiler can do.
+         */
+        int x = 100;
+        String Conversion = String.valueOf(x);
+        System.out.println(Conversion);
+        int y = 200;
+        String result = x + "+" + y;//Implicit conversion since "+" is used, then the conversion is implicit.
+        System.out.println(result);
+
     }
 }
