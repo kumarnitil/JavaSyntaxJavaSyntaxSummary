4a763c56bde55da49e6f1ebe66788ce154760e76
Kumar Nitil <kumarnitil@gmail.com>
2022-04-11 18:15:13 +0530

    Programming with Java Completed

--- src/basics/java/LearnMoreDataTypes.java
+++ src/basics/java/LearnMoreDataTypes.java
@@ -3,7 +3,27 @@ package basics.java;
 public class LearnMoreDataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		Primitive Types
+		byte, short, int, long
+		float, double
+		char
+		boolean
+		Primitive types represent data only -
+		Unable to provide methods for operating on that data
+		##################################################
+		Primitive Wrapper Class
+		Enhances the capabilities of the primitive data types
+		- Can hold primitive data value
+		- Provide Methods
+		- Enable Compatibility with richer aspects of the Java Type System
+		Each primitive type has its own wrapper class and hence we will have wrapper class for all the following
+		- the wrapper class is the same name itself except the fact that the first letter is capitalized
+		Byte, Short, Integer, Long
+		Float, Double
+		Char
+		Boolean
+		 */
 
 	}
 
